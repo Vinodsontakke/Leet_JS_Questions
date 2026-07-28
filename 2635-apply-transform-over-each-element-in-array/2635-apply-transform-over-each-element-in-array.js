9bd361b60f6ba4ae52250f3040624a1fdbad7927
Vinod Sontakke <122771176+Vinodsontakke@users.noreply.github.com>
2026-07-28 17:47:37 +0530

    Time: 50 ms (19.68%), Space: 54.1 MB (18.95%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -4,9 +4,9 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    let returnedArray = []
-  for(let i=0 ; i<arr.length;i++){
-   returnedArray[i] = fn(arr[i],i)
-  }  
-  return returnedArray
+    let result = []
+    arr.forEach((value,index)=>{
+        result.push(fn(value,index))
+    }) 
+  return result
 };
